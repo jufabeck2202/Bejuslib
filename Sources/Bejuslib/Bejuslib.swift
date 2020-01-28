f05621adcd286331e2405481f7b71a73a82a2c5f
Julian Beck <ju-fa-beck@t-online.de>
2020-01-28 22:14:47 +0100

    removed testfile

--- Sources/Bejuslib/Bejuslib.swift
+++
@@ -1,3 +0,0 @@
-struct Bejuslib {
-    var text = "Hello, World!"
-}
--- Tests/BejuslibTests/BejuslibTests.swift
+++ Tests/BejuslibTests/BejuslibTests.swift
@@ -6,10 +6,11 @@ final class BejuslibTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Bejuslib().text, "Hello, World!")
+        //XCTAssertEqual(Bejuslib().text, "Hello, World!")
     }
 
     static var allTests = [
         ("testExample", testExample),
     ]
 }
+
